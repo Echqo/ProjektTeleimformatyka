56205d0491ecd49e6d172d2844b900df1ab2d9fb
Owiec <miki.o@op.pl>
2017-04-02 01:22:58 +0200

    test odleglosci przy prekonfigurowanym serwerze, powalcze jutro z wyłuskaniem dB

---
+++ AP/AP/hd44780.c
@@ -0,0 +1,139 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>
+#include <stdlib.h>
+#include "hd44780.h"
+
+void rejestr(unsigned char data, unsigned char WriteOrErase)
+{
+if(WriteOrErase)
+tempdata=(tempdata|data);
+else
+tempdata&=~(data);
+
+ST_0;
+#if MODE==1
+	#if ENABLE_Q7_Q6==1
+		unsigned char local = 0x80;
+	#elif ENABLE_Q7_Q6==0
+		unsigned char local = 0x20;
+	#endif
+	while(local)
+	{
+	SH_0;
+	if(tempdata & local)DS_1; else DS_0;
+	SH_1;
+	local>>=1;
+	}
+#elif MODE==2
+	#if ENABLE_Q7_Q6==1
+	SH_0;
+	if(tempdata & 0x80)DS_1; else DS_0;
+	SH_1;
+	SH_0;
+	if(tempdata & 0x40)DS_1; else DS_0;
+	SH_1;	
+	# endif
+	
+	SH_0;
+	if(tempdata & 0x20)DS_1; else DS_0;
+	SH_1;
+	SH_0;
+	if(tempdata & 0x10)DS_1; else DS_0;
+	SH_1;
+	SH_0;
+	if(tempdata & 0x08)DS_1; else DS_0;
+	SH_1;
+	SH_0;
+	if(tempdata & 0x04)DS_1; else DS_0;
+	SH_1;
+	SH_0;
+	if(tempdata & 0x02)DS_1; else DS_0;
+	SH_1;
+	SH_0;
+	if(tempdata & 0x01)DS_1; else DS_0;
+	SH_1;
+	
+#endif
+ST_1;
+}
+
+void WriteToLCD (uint8_t v,unsigned char rs)
+{
+if(rs==1)RS_1; else RS_0;
+	
+	E_1;
+
+    if(v&0x10) D4_1; else D4_0;
+    if(v&0x20) D5_1; else D5_0;
+    if(v&0x40) D6_1; else D6_0;
+    if(v&0x80) D7_1; else D7_0;
+    E_0;  
+    _delay_ms(1.5);
+	
+	E_1;
+	
+    if(v&0x01) D4_1; else D4_0;
+    if(v&0x02) D5_1; else D5_0;
+    if(v&0x04) D6_1; else D6_0;
+    if(v&0x08) D7_1; else D7_0;
+    E_0;
+	_delay_ms(1.5);
+	
+	}
+void lcd_init(void)
+{
+
+DDRn|=(1<<DS_PIN)|(1<<ST_PIN)|(1<<SH_PIN);
+
+//WriteToLCD(0x02,LCDCOMMAND);	//cursor home (pos 0,0)
+
+LCD_FUNCTION_SET(LCD_FUNCTION_SET_D);
+LCD_DISPLAY(LCD_DISPLAY_D);
+cls;
+LCD_ENTRY_MODE_SET(LCD_ENTRY_MODE_SET_I);
+}
+void lcd_RAM(char *str)
+{
+    unsigned char i =0;
+
+    while(str[i])
+        LCD_WRITE_DATA(str[i++]) ;
+}
+void lcd_FLASH(const char *str)
+{
+unsigned char znak=0;
+	while((znak=pgm_read_byte(str++)))
+		LCD_WRITE_DATA(znak);
+}
+void lcd_int(uint8_t val,uint8_t system)
+{
+	char bufor[9];
+	itoa(val,bufor,system);
+	lcd_RAM(bufor);
+}
+void lcd_int16(uint16_t val,uint8_t system)
+{
+	char bufor[17];
+	ltoa(val,bufor,system);
+	lcd_RAM(bufor);
+}
+
+#ifdef USE_TEMP_CHAR
+void definiuj_stopien_Celsjusza(void)
+{
+	unsigned char tab[8]={6,9,9,6,0,0,0,0};//°
+	WriteToLCD(0x40,LCDCOMMAND);
+	for(uint8_t i=0;i<8;i++)
+	{
+		WriteToLCD(tab[i],LCDDATA);
+	}
+	WriteToLCD(0x00,LCDDATA);
+}
+/*
+	definiuj_stopien_Celsjusza();
+	pos(0,0);
+	WriteToLCD(0,LCDDATA);
+*/
+
+#endif
--- AP/AP/main.c
+++ AP/AP/main.c
@@ -6,14 +6,74 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdlib.h>
 #include "UART.h"
+#include "hd44780.h"
 
+volatile char znak;
+volatile uint8_t indeks_uart=0,flaga_uart=0;
+ volatile char bufor_uart[64];
+
+static inline uint8_t znajdz(const char *pattern);
+ISR(USART_RX_vect)
+{
+	znak=UDR0;
+	if(znak==10 || znak ==13)
+	{
+		if(indeks_uart)
+		{
+			flaga_uart=1;
+			indeks_uart=0;
+		}
+	}
+	if(znak!=13 && znak !=10)
+	{
+		bufor_uart[indeks_uart++]=znak;
+		indeks_uart &= 0x3f;		//utnij gdyby indeks > 63
+	}
+}
 int main(void)
 {
 	UART_Init();
-    /* Replace with your application code */
+	lcd_init();
+	sei();
+  
+//	UART_Write("AT+CIPSTART=\"TCP\",\"192.168.4.0\",81");UART_crlf();
+//	_delay_ms(1000);
+	lcd("start");
+	UART_Write("AT+CWJAP?");UART_crlf();
+	DDRC|=1;
+	uint8_t *ptr;
+	
     while (1) 
     {
+		if(flaga_uart)
+		{
+			flaga_uart=0;
+			if((ptr=znajdz("CWJAP")))
+			{
+				//pos(0,0);
+				PORTC^=1;
+				//ptr+=12;
+				lcd_RAM(ptr);
+				
+				//UART_Write(*ptr);
+				_delay_ms(1000);
+				UART_Write("AT+CWJAP?");UART_crlf();	
+				
+			}
+		//	
+		}
+		
+		
     }
 }
 
+static inline uint8_t znajdz(const char *pattern)
+{
+	if(strstr(bufor_uart,pattern))
+	return 1;
+	return 0;
+}
